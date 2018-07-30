ad864cf18bd9a0f054fb2965b1098e59025b0572
Eric Lippmann <eric.lippmann@icinga.com>
2018-07-30 16:49:58 +0200

    Provide hook Pdfexport

---
+++ library/Pdfexport/ProvidedHook/Pdfexport.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Icinga\Module\Pdfexport\ProvidedHook;
+
+use Icinga\Application\Config;
+use Icinga\Application\Hook\PdfexportHook;
+use Icinga\Application\Icinga;
+use Icinga\Module\Pdfexport\HeadlessChrome;
+
+class Pdfexport extends PdfexportHook
+{
+    public static function getBinary()
+    {
+        return Config::module('pdfexort')->get('chrome', 'binary', '/bin/google-chrome');
+    }
+
+    public function isSupported()
+    {
+        return (new HeadlessChrome())->setBinary(static::getBinary())->getVersion() >= 59;
+    }
+
+    public function streamPdfFromHtml($html, $filename)
+    {
+        $filename = basename($filename, '.pdf') . '.pdf';
+
+        $style = strpos($html, '</style>');
+        if ($style !== false) {
+            // Inject custom CSS
+            $css = <<<'CSS'
+@page {
+  margin: 1.6cm;
+}
+
+body {
+  margin: 0;
+}
+
+body > img {
+  margin-top: -1em;
+}
+</style>
+CSS;
+            $html = substr($html, 0, $style) . $css . substr($html, $style + 8);
+        }
+
+        $footer = strrpos($html, '<div id="page-footer">');
+        if ($footer !== false) {
+            // Hide page footer
+            $html = substr($html, 0, $footer)
+                . '<div id="page-footer" style="display: none !important;">'
+                . substr($html, $footer + 22);
+        }
+
+        // Keep reference to the chrome object because it is using temp files which are automatically removed when
+        // the object is destructed
+        $chrome = new HeadlessChrome();
+
+        $pdf = $chrome
+            ->setBinary(static::getBinary())
+            ->fromHtml($html)
+            ->toPdf($filename);
+
+        $response = Icinga::app()->getResponse();
+
+        $response->setHeader('Content-Type', 'application/pdf', true);
+        $response->setHeader('Content-Disposition', "inline; filename=\"$filename\"", true);
+        $response->sendHeaders();
+
+        readfile($pdf);
+
+        exit;
+    }
+}
