b8b2c62b83cbae5a9dcbffd49529d536669eb531
Eric Lippmann <eric.lippmann@icinga.com>
2019-01-24 15:53:41 +0100

    Add function htmlToPdf()

--- library/Pdfexport/ProvidedHook/Pdfexport.php
+++ library/Pdfexport/ProvidedHook/Pdfexport.php
@@ -19,6 +19,48 @@ class Pdfexport extends PdfexportHook
         return (new HeadlessChrome())->setBinary(static::getBinary())->getVersion() >= 59;
     }
 
+    public function htmlToPdf($html)
+    {
+        $style = strpos($html, '</style>');
+        if ($style !== false) {
+            // Inject custom CSS
+            $css = <<<'CSS'
+@page {
+  margin: 1.6cm;
+}
+
+body {
+  margin: 0;
+}
+
+body > img {
+  margin-top: -1em;
+}
+</style>
+CSS;
+            $html = substr($html, 0, $style) . $css . substr($html, $style + 8);
+        }
+
+        $footer = strrpos($html, '<div id="page-footer">');
+        if ($footer !== false) {
+            // Hide page footer
+            $html = substr($html, 0, $footer)
+                . '<div id="page-footer" style="display: none !important;">'
+                . substr($html, $footer + 22);
+        }
+
+        // Keep reference to the chrome object because it is using temp files which are automatically removed when
+        // the object is destructed
+        $chrome = new HeadlessChrome();
+
+        $pdf = $chrome
+            ->setBinary(static::getBinary())
+            ->fromHtml($html)
+            ->toPdf('direct');
+
+        return file_get_contents($pdf);
+    }
+
     public function streamPdfFromHtml($html, $filename)
     {
         $filename = basename($filename, '.pdf') . '.pdf';
