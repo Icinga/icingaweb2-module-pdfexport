d8d4f989cefdd399aa450283fcf245c47601e7df
Johannes Meyer <johannes.meyer@icinga.com>
2020-01-08 15:57:09 +0100

    HeadlessChrome: Add support for a remote Chrome

--- library/Pdfexport/HeadlessChrome.php
+++ library/Pdfexport/HeadlessChrome.php
@@ -8,6 +8,7 @@ use Icinga\Application\Logger;
 use Icinga\Application\Platform;
 use Icinga\File\Storage\StorageInterface;
 use Icinga\File\Storage\TemporaryLocalFileStorage;
+use LogicException;
 use React\ChildProcess\Process;
 use React\EventLoop\Factory;
 use React\EventLoop\TimerInterface;
@@ -25,6 +26,9 @@ class HeadlessChrome
 
     /** @var string Path to the Chrome binary */
     protected $binary;
+
+    /** @var array Host and port to the remote Chrome */
+    protected $remote;
 
     /**
      * The document to print
@@ -59,6 +63,31 @@ class HeadlessChrome
     public function setBinary($binary)
     {
         $this->binary = $binary;
+
+        return $this;
+    }
+
+    /**
+     * Get host and port combination of the remote chrome
+     *
+     * @return array
+     */
+    public function getRemote()
+    {
+        return $this->remote;
+    }
+
+    /**
+     * Set host and port combination of a remote chrome
+     *
+     * @param string $host
+     * @param int    $port
+     *
+     * @return $this
+     */
+    public function setRemote($host, $port)
+    {
+        $this->remote = [$host, $port];
 
         return $this;
     }
@@ -163,7 +192,7 @@ class HeadlessChrome
             $html = $this->document->render();
         }
 
-        if ($asFile) {
+        if ($asFile && $this->remote === null) {
             $path = uniqid('icingaweb2-pdfexport-') . '.html';
             $storage = $this->getFileStorage();
 
@@ -187,64 +216,72 @@ class HeadlessChrome
      */
     public function toPdf()
     {
-        $browserHome = $this->getFileStorage()->resolvePath('HOME');
-        $commandLine = join(' ', [
-            escapeshellarg($this->getBinary()),
-            static::renderArgumentList([
-                '--bwsi',
-                '--headless',
-                '--disable-gpu',
-                '--no-sandbox',
-                '--no-first-run',
-                '--disable-dev-shm-usage',
-                '--remote-debugging-port=0',
-                '--homedir=' => $browserHome,
-                '--user-data-dir=' => $browserHome
-            ])
-        ]);
-
-        if (Platform::isLinux()) {
-            Logger::debug('Starting browser process: HOME=%s exec %s', $browserHome, $commandLine);
-            $chrome = new Process('exec ' . $commandLine, null, ['HOME' => $browserHome]);
+        if ($this->remote !== null) {
+            $result = $this->jsonVersion($this->remote[0], $this->remote[1]);
+            $parts = explode('/', $result['webSocketDebuggerUrl']);
+            $pdf = $this->printToPDF(join(':', $this->remote), end($parts), isset($this->document)
+                ? $this->document->getPrintParameters()
+                : []);
         } else {
-            Logger::debug('Starting browser process: %s', $commandLine);
-            $chrome = new Process($commandLine);
-        }
+            $browserHome = $this->getFileStorage()->resolvePath('HOME');
+            $commandLine = join(' ', [
+                escapeshellarg($this->getBinary()),
+                static::renderArgumentList([
+                    '--bwsi',
+                    '--headless',
+                    '--disable-gpu',
+                    '--no-sandbox',
+                    '--no-first-run',
+                    '--disable-dev-shm-usage',
+                    '--remote-debugging-port=0',
+                    '--homedir=' => $browserHome,
+                    '--user-data-dir=' => $browserHome
+                ])
+            ]);
 
-        $loop = Factory::create();
+            if (Platform::isLinux()) {
+                Logger::debug('Starting browser process: HOME=%s exec %s', $browserHome, $commandLine);
+                $chrome = new Process('exec ' . $commandLine, null, ['HOME' => $browserHome]);
+            } else {
+                Logger::debug('Starting browser process: %s', $commandLine);
+                $chrome = new Process($commandLine);
+            }
 
-        $killer = $loop->addTimer(10, function (TimerInterface $timer) use ($chrome) {
-            $chrome->terminate(6); // SIGABRT
-            Logger::error(
-                'Terminated browser process after %d seconds elapsed without the expected output',
-                $timer->getInterval()
-            );
-        });
+            $loop = Factory::create();
 
-        $chrome->start($loop);
+            $killer = $loop->addTimer(10, function (TimerInterface $timer) use ($chrome) {
+                $chrome->terminate(6); // SIGABRT
+                Logger::error(
+                    'Terminated browser process after %d seconds elapsed without the expected output',
+                    $timer->getInterval()
+                );
+            });
 
-        $pdf = null;
-        $chrome->stderr->on('data', function ($chunk) use (&$pdf, $chrome, $loop, $killer) {
-            Logger::debug('Caught browser output: %s', $chunk);
+            $chrome->start($loop);
 
-            if (preg_match(self::DEBUG_ADDR_PATTERN, trim($chunk), $matches)) {
+            $pdf = null;
+            $chrome->stderr->on('data', function ($chunk) use (&$pdf, $chrome, $loop, $killer) {
+                Logger::debug('Caught browser output: %s', $chunk);
+
+                if (preg_match(self::DEBUG_ADDR_PATTERN, trim($chunk), $matches)) {
+                    $loop->cancelTimer($killer);
+
+                    $pdf = $this->printToPDF($matches[1], $matches[2], isset($this->document)
+                        ? $this->document->getPrintParameters()
+                        : []);
+
+                    $chrome->terminate();
+                }
+            });
+
+            $chrome->on('exit', function ($exitCode, $termSignal) use ($loop, $killer) {
                 $loop->cancelTimer($killer);
 
-                $pdf = $this->printToPDF($matches[1], $matches[2], isset($this->document)
-                    ? $this->document->getPrintParameters()
-                    : []);
+                Logger::debug('Browser terminated by signal %d and exited with code %d', $termSignal, $exitCode);
+            });
 
-                $chrome->terminate();
-            }
-        });
-
-        $chrome->on('exit', function ($exitCode, $termSignal) use ($loop, $killer) {
-            $loop->cancelTimer($killer);
-
-            Logger::debug('Browser terminated by signal %d and exited with code %d', $termSignal, $exitCode);
-        });
-
-        $loop->run();
+            $loop->run();
+        }
 
         if (empty($pdf)) {
             throw new Exception(
@@ -431,21 +468,54 @@ class HeadlessChrome
      */
     public function getVersion()
     {
-        $command = new ShellCommand(
-            escapeshellarg($this->getBinary()) . ' ' . static::renderArgumentList(['--version']),
-            false
-        );
+        switch (true)
+        {
+            case $this->remote !== null:
+                $result = $this->jsonVersion($this->remote[0], $this->remote[1]);
+                $version = $result['Browser'];
+                break;
+            case $this->binary !== null:
+                $command = new ShellCommand(
+                    escapeshellarg($this->getBinary()) . ' ' . static::renderArgumentList(['--version']),
+                    false
+                );
 
-        $output = $command->execute();
+                $output = $command->execute();
 
-        if ($command->getExitCode() !== 0) {
-            throw new \Exception($output->stderr);
+                if ($command->getExitCode() !== 0) {
+                    throw new \Exception($output->stderr);
+                }
+
+                $version = $output->stdout;
+                break;
+            default:
+                throw new LogicException('Set a binary or remote first');
         }
 
-        if (preg_match('/\s(\d+)\.[\d\.]+\s/', $output->stdout, $match)) {
+        if (preg_match('/(\d+)\.[\d.]+/', $version, $match)) {
             return (int) $match[1];
         }
 
         return false;
+    }
+
+    /**
+     * Fetch result from the /json/version API endpoint
+     *
+     * @param string $host
+     * @param int    $port
+     *
+     * @return bool|array
+     */
+    protected function jsonVersion($host, $port)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', sprintf('http://%s:%s/json/version', $host, $port));
+
+        if ($response->getStatusCode() !== 200) {
+            return false;
+        }
+
+        return json_decode($response->getBody(), true);
     }
 }
