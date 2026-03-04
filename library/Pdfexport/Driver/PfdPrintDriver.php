42fb2174305f78b713af29b27bb9da03c5086c2c
Alexander Rieß <alexander.riess@icinga.com>
2026-03-04 14:33:18 +0100

    Use destructor instead of close method

---
+++ library/Pdfexport/Driver/PfdPrintDriver.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Icinga\Module\Pdfexport\Driver;
+
+use Icinga\Module\Pdfexport\PrintableHtmlDocument;
+
+interface PfdPrintDriver
+{
+    function toPdf(PrintableHtmlDocument $document): string;
+}
